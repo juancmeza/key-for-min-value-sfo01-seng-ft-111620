989aeb0900138234bbd04f907bf6520a26f50f2e
searching-block-4535 <juancmezat@gmail.com>
2020-10-31 05:35:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.empty?
+  
+  smallest = 10000
+  smallest_key = :empty
+  name_hash.each do |key, value|
+    if value < smallest
+      smallest = value
 
 end
